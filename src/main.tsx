c27f04159cfe3a48547173911dbdf75ab06a89d0
clublasmorasde <tradingcoarg@gmail.com>
2026-02-05 14:57:57 -0300

    Fix rendering root element in main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Esto buscará automáticamente tu App.jsx en la misma carpeta
-import './index.css'   // Esto cargará Tailwind
+import App from './App'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
