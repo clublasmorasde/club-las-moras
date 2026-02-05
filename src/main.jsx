76f6e67f2cab99f44071bb577dca40a0941037fe
clublasmorasde <tradingcoarg@gmail.com>
2026-02-05 21:37:44 +0000

    Fix: corrigiendo importaciones y nombres de archivos

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App' // Asegúrate de que la 'A' sea mayúscula como en tu archivo
+import App from './App' // Debe coincidir con el nombre del archivo
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
