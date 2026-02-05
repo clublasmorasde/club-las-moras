3c1ac59a29f4bfbff9d0014c89a03b7f55d96552
clublasmorasde <tradingcoarg@gmail.com>
2026-02-05 14:50:04 -0300

    Modify imports in main.tsx for App and CSS
    Updated import statement for App component and added Tailwind CSS import.

--- main.tsx
+++ main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App' // Esto buscará automáticamente tu App.jsx en la misma carpeta
+import './index.css'   // Esto cargará Tailwind
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
