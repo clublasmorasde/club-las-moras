eef33833bdbb230b0f1327615a28d3987e028945
clublasmorasde <tradingcoarg@gmail.com>
2026-02-05 21:35:55 +0000

    Final fix: sincronizando nombres y scripts

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App' // Asegúrate de que la 'A' sea mayúscula como en tu archivo
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
