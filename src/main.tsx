939f1f0a5c3dcc74facaf2ef316b54341be7936c
clublasmorasde <tradingcoarg@gmail.com>
2026-02-05 14:55:54 -0300

    Set up React application with root rendering

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App' // Esto buscará automáticamente tu App.jsx en la misma carpeta
+import './index.css'   // Esto cargará Tailwind
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
